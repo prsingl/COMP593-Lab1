50cbe3452e03683bcc77e21862ad1b48f63d797d
Priyanshu singla <priyanshu.singla@flemingcollege.ca>
2024-05-24 18:49:12 -0400

    Modified both files in the repo clone

--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 #Print this line from Zen of Python 
 print("Although that way may not be obvious at first unless you're Dutch.")
+print("Hey Buddy, Nice to meet You.")
+print("My name is Priyanshu Singla")
